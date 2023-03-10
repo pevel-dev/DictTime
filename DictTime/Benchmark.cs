5592761b8d85c8c16c1324a49c4eea6c99ec3cf8
pevel-dev <kkollop123@gmail.com>
2023-03-10 10:41:57 +0500

    Initial commit

---
+++ DictTime/Benchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using NUnit.Framework;
+
+namespace Recognizer
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod1()
+        {            
+            var config = new ManualConfig()
+                .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+                .AddValidator(JitOptimizationsValidator.DontFailOnError)
+                .AddLogger(ConsoleLogger.Default)
+                .AddColumnProvider(DefaultColumnProviders.Instance);
+            BenchmarkRunner.Run<BenchmarkTest>(config);
+        }
+    }
+}
---
+++ DictTime/BenchmarkTest.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Recognizer;
+
+public class BenchmarkTest
+{
+    [Benchmark(Description = "Range")]
+    public void RangeInts()
+    {
+        
+    }
+    
+    [Benchmark(Description = "Broken")]
+    public void BrokenInts()
+    {
+        
+    }
+}
